4272e7ebfab73dee81df4bc8d9881b5d9d7f372d
sevenuncler <easycynly@126.com>
2017-11-22 08:12:15 +0800

    Initial Commit

---
+++ selfish/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  selfish
+//
+//  Created by He on 2017/11/22.
+//  Copyright © 2017年 He. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ selfish/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  selfish
+//
+//  Created by He on 2017/11/22.
+//  Copyright © 2017年 He. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
